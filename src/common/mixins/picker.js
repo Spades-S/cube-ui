0eccab620ad25f751c3430bf0a90b0542085f785
Spades-S <allengefeng@163.com>
2018-05-04 15:17:58 +0800

    Delete useless files

---
+++ src/common/mixins/picker.js
@@ -0,0 +1,19 @@
+export default {
+  props: {
+    title: {
+      type: String
+    },
+    cancelTxt: {
+      type: String,
+      default: '取消'
+    },
+    confirmTxt: {
+      type: String,
+      default: '确定'
+    },
+    swipeTime: {
+      type: Number,
+      default: 2500
+    }
+  }
+}
